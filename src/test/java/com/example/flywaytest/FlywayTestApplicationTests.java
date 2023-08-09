32cad3ab54706da764ac8d281fbc4cebc79eb1db
jiyong.yoon <jyyoon0615@naver.com>
2023-08-09 09:50:12 +0900

    git init

---
+++ src/test/java/com/example/flywaytest/FlywayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flywaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
